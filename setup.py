9545854756af92de87af6fe39d39d1ee2e6c273d
evanweenen <evanweenen@ethz.ch>
2023-01-12 17:29:43 +0100

    Change version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='glucose-stats',
-    version='0.1.0',
+    version='0.0.1',
     author='Eva van Weenen',
     author_email='evanweenen@ethz.ch',
     description='Calculate glucose statistics from continuous glucose monitoring data',
