a1592291e3db7d40de8527cb0009aed5fd6ee2fb
evanweenen <evanweenen@ethz.ch>
2023-01-12 17:22:26 +0100

    Include setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='glucose-stats',
+    version='0.1.0',
+    author='Eva van Weenen',
+    author_email='evanweenen@ethz.ch',
+    description='Calculate glucose statistics from continuous glucose monitoring data',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/evavanweenen/glucose-stats',
+    license='MIT',
+    packages=['glucosestats'],
+    scripts=['bin/run.py'],
+    zip_safe=False,
+    install_requires=['numpy', 'pandas','pytorch','matplotlib','seaborn']
+    )
